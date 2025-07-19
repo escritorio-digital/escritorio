2c8734630647e27b94b656f25cf3b9d215c542b4
Pablo G Guizar <pablogguizar@gmail.com>
2025-07-18 20:08:33 -0600

    fix(build): Añadir plugin commonjs a Vite para corregir error de 'require'

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import commonjs from '@rollup/plugin-commonjs'; // <-- 1. Importar desde la ubicación correcta
 
+// https://vite.dev/config/
 export default defineConfig({
   base: '/escritorio-interactivo-aula/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    commonjs(), 
+  ],
 })
